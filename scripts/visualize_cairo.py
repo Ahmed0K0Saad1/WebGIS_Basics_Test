ecca5140c1034c325e34eed20ff2bb54c74244ae
Ahmed <ahmed@example.com>
2026-06-03 07:38:22 +0300

    Highlight Cairo governorate in visualization

---
+++ scripts/visualize_cairo.py
@@ -0,0 +1,11 @@
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+gdf = gpd.read_file("data/gadm41_EGY_1.shp")
+
+fig, ax = plt.subplots(figsize=(10, 10))
+gdf.plot(edgecolor="black", facecolor="lightblue", ax=ax)
+
+selected_gov = 'Al Qahirah'
+gdf[gdf['NAME_1'] == selected_gov].plot(edgecolor="red", facecolor="yellow", ax=ax)
+plt.show()
