6dce496a47842ae3ab76ec4b084b5433c1cf58a8
Ahmed <ahmed@example.com>
2026-06-06 06:58:41 +0300

    Add cairo_map.png

--- docs/notes.md
+++
@@ -1,5 +0,0 @@
-# Notes - Week 1
-
-- Successfully read Egypt administrative boundaries using GeoPandas.
-- Data files: gadm41_EGY_0, gadm41_EGY_1, gadm41_EGY_2.
-- Next step: visualize boundaries and start filtering by governorates.
--- scripts/visualize_cairo.py
+++ scripts/visualize_cairo.py
@@ -1,6 +1,6 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
-
+import os
 gdf = gpd.read_file("data/gadm41_EGY_1.shp")
 
 fig, ax = plt.subplots(figsize=(10, 10))
@@ -9,3 +9,5 @@ gdf.plot(edgecolor="black", facecolor="lightblue", ax=ax)
 selected_gov = 'Al Qahirah'
 gdf[gdf['NAME_1'] == selected_gov].plot(edgecolor="red", facecolor="yellow", ax=ax)
 plt.show()
+
+fig.savefig(os.path.join("output", "cairo_map.png"), dpi=300)
